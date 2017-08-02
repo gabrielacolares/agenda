4e028555c75b2f0e32d593b36cd6f392b5d7f409
GEINF02MAC <administrador@iMac-de-GEINF02MAC.local>
2017-08-02 16:32:03 -0400

    Lista de Contatos Mocada

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -5,22 +5,41 @@ app.controller('HomeCtrl', function($rootScope, $location)
 
 });
 
-app.controller('CadastroCtrl', function($rootScope, $location)
+app.controller('CadastroCtrl', function($rootScope, $location,$http)
 {
    $rootScope.activetab = $location.path();
-    console.log( $rootScope.contato);
-    $rootScope.enviar = function(contato){}
+   $rootScope.contato = {};
+   $rootScope.contato.date =  new Date();
+   console.log($rootScope.contato.date);
+   $rootScope.mensagem = '';
+   $rootScope.submeter = function() {
+       
+
+            // $http.get('http://demo8534877.mockable.io/contatos', $rootScope.contato).success(function() {
+                $rootScope.mensagem = 'Foto adicionada com sucesso';
+            // }).error(function(erro) {
+            //     console.log('Não foi possível cadastrar a foto');
+            // })
+        };
+
 
 });
 
 
-app.controller('ListagemCtrl', function($rootScope, $location)
+app.controller('ListagemCtrl', function($rootScope, $location,$http)
 {
-   $rootScope.activetab = $location.path();
-   $rootScope.contatos = [{nome:'gabi'},{nome:'luciana'}];
-   $rootScope.remover = function(contato) {}
-   
-});
+    $rootScope.activetab = $location.path();
+    $rootScope.contatos = [];
+    $http.get('http://demo8534877.mockable.io/contatos').
+        then(function(response) {
+            $rootScope.contatos = response.data.contatos;
+        });
+
+        
+    //    $rootScope.remover = function(contato) {
+    //    }
+    
+    });
 app.controller('DocumentacaoCtrl', function($rootScope, $location)
 {
    $rootScope.activetab = $location.path();
